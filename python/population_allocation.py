6888281ebcabf2ddf5ac454daf0c90c38e666efe
Silas Schweizer <silas@ik.me>
2024-11-25 13:38:09 +0100

    debugging and improved logging

--- python/population_allocation.py
+++ python/population_allocation.py
@@ -18,33 +18,47 @@ def allocate_population(flat_db_path, population_polygon_path):
     try:
         logging.info("Loading flat database.")
         flats = gpd.read_file(flat_db_path)
-
         logging.info("Loading population polygons.")
         population = gpd.read_file(population_polygon_path)
-
+        # -9999 values are nodata/0 in all colunns
+        logging.info("Replacing -9999 values with 0.")
+        population = population.replace(-999, 0)
         logging.info("Ensuring CRS match.")
         if flats.crs != population.crs:
             flats = flats.to_crs(population.crs)
 
         logging.info("Spatial joining flats with population polygons.")
-        flats = gpd.sjoin(flats, population, how='left', op='within')
-
+        flats = gpd.sjoin(flats, population, how='left', predicate='within')
+        logging.info(f"Spatial join completed, length of flats: {len(flats)}")
+        flats.query('wstatlang == "Bestehend"', inplace=True)
         logging.info("Allocating population to flats based on 'wazim'.")
-        # grouped variable removed as it is not used
-
         est_pop = np.zeros(len(flats))
 
         for idx, row in flats.iterrows():
             # cell_pop variable removed as it is not used
-            wazim = row['wazim']
-            est_pop[idx] = wazim  # Allocate population based on number of rooms in the flats
+            # Calculate total population and total wazim in the current cell
+            cell_pop = row['PERS_N']  
+            cell_wazim = flats[flats['index_right'] == row['index_right']]['wazim'].sum()
+            
+            # Compute average number of people per wazim
+            avg_people_per_wazim = cell_pop / cell_wazim if cell_wazim != 0 else 0
+            
+            # Allocate population based on average people per wazim
+            est_pop[idx] = row['wazim'] * avg_people_per_wazim
 
-            #logging.info(f"Flat ID {row['egid']}: Allocated population {est_pop[idx]}")
+            logging.info(f"Flat ID {row['egid']}: Allocated population {est_pop[idx]}")
+
+        #global average population per wazim
+        avg_pop_per_wazim = est_pop.sum() / flats['wazim'].sum()
+        logging.info(f"Average population per wazim: {avg_pop_per_wazim}")
+        # Replace NaN values with the average population per wazim
+        est_pop[np.isnan(est_pop)] = avg_pop_per_wazim * flats['wazim'][np.isnan(est_pop)]
 
         flats['est_pop'] = est_pop
         logging.info("Population allocation completed.")
         flats.to_file(OUTPUT_PATH)
         logging.info(f"Allocated population saved to {OUTPUT_PATH}")
+        logging.info("Total population allocated: ", flats['est_pop'].sum())
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
