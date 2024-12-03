7801e04ac1b7ec68dd4b92ef7fc8a343e704551d
Silas Schweizer <silas@ik.me>
2024-12-03 14:56:36 +0100

    update environment configuration and refactor logging in scripts to use snakemake logger

--- scripts/population_allocation.py
+++ scripts/population_allocation.py
@@ -1,36 +1,30 @@
-import logging
 import geopandas as gpd
 import numpy as np
+from snakemake.logging import logger
 
 # Remove hardcoded paths and configure logging using snakemake variables
 flat_db_path = snakemake.input['flats']
 population_polygon_path = snakemake.input['population']
 output_path = snakemake.output[0]
 
-logging.basicConfig(
-    filename=snakemake.log[0],
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
-
 def allocate_population(flat_db_path, population_polygon_path, output_path):
     try:
-        logging.info("Loading flat database.")
+        logger.info("Loading flat database.")
         flats = gpd.read_file(flat_db_path)
-        logging.info("Loading population polygons.")
+        logger.info("Loading population polygons.")
         population = gpd.read_file(population_polygon_path)
-        # -9999 values are nodata/0 in all colunns
-        logging.info("Replacing -9999 values with 0.")
+        # -9999 values are nodata/0 in all columns
+        logger.info("Replacing -9999 values with 0.")
         population = population.replace(-999, 0)
-        logging.info("Ensuring CRS match.")
+        logger.info("Ensuring CRS match.")
         if flats.crs != population.crs:
             flats = flats.to_crs(population.crs)
 
-        logging.info("Spatial joining flats with population polygons.")
+        logger.info("Spatial joining flats with population polygons.")
         flats = gpd.sjoin(flats, population, how='left', predicate='within')
-        logging.info(f"Spatial join completed, length of flats: {len(flats)}")
+        logger.info(f"Spatial join completed, length of flats: {len(flats)}")
         flats.query('wstatlang == "Bestehend"', inplace=True)
-        logging.info("Allocating population to flats based on 'wazim'.")
+        logger.info("Allocating population to flats based on 'wazim'.")
         est_pop = np.zeros(len(flats))
 
         for i, (_, row) in enumerate(flats.iterrows()):
@@ -45,22 +39,22 @@ def allocate_population(flat_db_path, population_polygon_path, output_path):
             # Allocate population based on average people per wazim
             est_pop[i] = row['wazim'] * avg_people_per_wazim
 
-            #logging.info(f"Flat ID {row['egid']}: Allocated population {est_pop[idx]}")
+            #logger.info(f"Flat ID {row['egid']}: Allocated population {est_pop[idx]}")
 
-        #global average population per wazim
+        # Global average population per wazim
         avg_pop_per_wazim = est_pop.sum() / flats['wazim'].sum()
-        logging.info(f"Average population per wazim: {avg_pop_per_wazim}")
+        logger.info(f"Average population per wazim: {avg_pop_per_wazim}")
         # Replace NaN values with the average population per wazim
         est_pop[np.isnan(est_pop)] = avg_pop_per_wazim * flats['wazim'][np.isnan(est_pop)]
 
         flats['est_pop'] = est_pop
-        logging.info("Population allocation completed.")
+        logger.info("Population allocation completed.")
         flats.to_file(output_path)
-        logging.info(f"Allocated population saved to {output_path}")
-        logging.info(f"Total population allocated: {flats['est_pop'].sum()}")
+        logger.info(f"Allocated population saved to {output_path}")
+        logger.info(f"Total population allocated: {flats['est_pop'].sum()}")
 
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     allocate_population(flat_db_path, population_polygon_path, output_path)
