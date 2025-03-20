7686c442e2b901251ebee8c3d105f9313a360c9e
Silas <silas@ik.me>
2025-03-20 16:48:55 +0100

    changed range of n for sensitivity analysis

--- scripts/sensitivity_analysis.py
+++ scripts/sensitivity_analysis.py
@@ -8,7 +8,7 @@ import matplotlib.ticker as mticker
 
 # Define cluster numbers and output path
 
-CLUSTERS = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250, 300, 350, 400, 500, 600, 700, 800, 900, 1000, 1500]
+CLUSTERS = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000]
 DERIVED_DATA = Path("/home/silas/rcp_project/rcp_project/data/derived_data")
 PLOTS_PATH = Path("/home/silas/rcp_project/rcp_project/data/plots")
 
